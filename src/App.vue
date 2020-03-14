d3aa0a753c561ac85021d693b91311feda06fdee
francescodist <francesco.dstf@gmail.com>
2020-03-14 11:38:18 +0100

    Add Home View

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
@@ -13,8 +13,14 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+
+body {
+  margin: 0;
 }
 
 #nav {
@@ -28,5 +34,13 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.page-container {
+  height: 100%;
+  max-height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,89 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="page-container">
+    <div class="home-title-container">Retro Board</div>
+    <div class="home-container">
+      <div class="home-item">
+        <input v-model="code" class="retro-code-input" placeholder="Retro Code Here..." />
+        <router-link :to="{path: '/board/' + code}" class="home-button" tag="button">Go</router-link>
+      </div>
+      <div class="home-item">
+        <router-link to="/board" class="home-button" tag="button">Start a Retro!</router-link>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+<style scoped>
+.home-title-container {
+  font-size: 64px;
+  font-weight: bold;
+  margin: auto;
+  text-align: center;
 }
+
+.home-container {
+  width: 600px;
+  max-width: 80%;
+  margin: auto auto calc(50vh - 120px) auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.home-item {
+  height: 60px;
+  margin: 5px;
+  display: flex;
+}
+
+.home-button {
+  background: #42b983;
+  color: white;
+  font-weight: bold;
+  height: 100%;
+  width: 100%;
+  font-size: 32px;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  transition: background-color 0.2s;
+}
+
+.home-button:hover {
+  background: #42b98381;
+}
+
+.home-button:active {
+  background: #42b983;
+}
+
+.home-button:focus {
+  background: #42b983;
+}
+
+.retro-code-input {
+  color: #42b983;
+  font-weight: bold;
+  font-size: 32px;
+  padding-left: 16px;
+  min-width: 70%;
+}
+
+.retro-code-input::placeholder {
+  color: #42b98381;
+}
+.retro-code-input:focus {
+  outline-color: #42b983;
+}
+</style>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      code: ""
+    };
+  }
+};
 </script>
